9029e24d1de72901d2392ae8f8a80a2e791f44e3
cheemapurp <71794424+cheemapurp@users.noreply.github.com>
2020-09-30 17:58:58 -0400

    Average program

--- average.py
+++ average.py
@@ -2,9 +2,10 @@ numberOfScores = score = total = scoreCount = 0
 average = 0.0
 numberOfScores = int(input("Please enter the number of scores you would like to average: "))
 
-score = int(input("Please enter a score: "))
-total += score
-scoreCount += 1
+while scoreCount != numberOfScores:
+    score = int(input("Please enter a score: "))
+    total += score
+    scoreCount += 1
 
 average = total / numberOfScores
-print(average)
+print("The average is " + format(average))
