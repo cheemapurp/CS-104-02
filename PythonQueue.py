c6d23c1c0e312731d4aaba63bd30336a71ebcda2
cheemapurp <71794424+cheemapurp@users.noreply.github.com>
2020-11-30 09:33:46 -0500

    Python Queue Assignment

---
+++ PythonQueue.py
@@ -0,0 +1,10 @@
+
+names = []
+for acceptedName in range(0,9):
+    acceptedName = input("Enter your name: ")
+    names.append(acceptedName)
+
+for acceptedName in names:
+    print(len(names)
+    print(names.pop(0))
+    
